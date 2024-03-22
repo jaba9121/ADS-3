ba8d70933fb2d6d6b749a784e6c110fa83f0598e
jaba9121 <160114001+jaba9121@users.noreply.github.com>
2024-03-22 09:20:26 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,12 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
+  int count = 0;
+  for (int i = 0; i < size; i++) {
+    if (arr[i] == value)
+      count++;
+  }
+  if (count > 0)
+    return count;
   return 0; // если ничего не найдено
 }
